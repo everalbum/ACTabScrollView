c6cfff271138df7918ee9ba1dd8f14bd58b49685
Charlie Melbye <charles.melbye@gmail.com>
2017-01-25 16:23:38 -0800

    Switch product to framework instead of app

---
+++ ACTabScrollView/ACTabScrollView.h
@@ -0,0 +1,19 @@
+//
+//  ACTabScrollView.h
+//  ACTabScrollView
+//
+//  Created by Charles Melbye on 1/25/17.
+//  Copyright © 2017 AzureChen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ACTabScrollView.
+FOUNDATION_EXPORT double ACTabScrollViewVersionNumber;
+
+//! Project version string for ACTabScrollView.
+FOUNDATION_EXPORT const unsigned char ACTabScrollViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ACTabScrollView/PublicHeader.h>
+
+
